bab0edbd017aee288d345d4dac8a1ef4be1d31c2
somudas <soumyadeep4122002@gmail.com>
2022-11-12 23:31:09 +0530

    product schema added

--- cosx_home/models.py
+++ cosx_home/models.py
@@ -4,5 +4,13 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
-
+class Products(models.Model):
+    p_name = models.CharField(max_length=100)
+    p_rating = models.DecimalField(max_digits=5, decimal_places=1)
+    p_description = models.CharField(max_length=200)
+    p_buycount = models.IntegerField()
+    p_brand = models.CharField(max_length=100)
+    p_category = models.CharField(max_length=100)
+    p_image = models.ImageField(upload_to="cosx_home/images/")
+    
 
